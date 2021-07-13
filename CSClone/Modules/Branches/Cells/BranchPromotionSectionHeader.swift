3697de389494a77d12362068b3e8ff26a36df09f
Samuel Garcia <samuel.garcia@cornershopapp.com>
2021-07-13 12:47:39 -0400

    add caption on promotion

--- CSClone/Modules/Branches/Cells/BranchPromotionSectionHeader.swift
+++ CSClone/Modules/Branches/Cells/BranchPromotionSectionHeader.swift
@@ -10,9 +10,11 @@ import UIKit
 class BranchPromotionSectionHeader: UICollectionReusableView {
     static let reuseIdentifier = "BranchPromotionSectionHeader"
     let imageView: UIImageView!
+    let titleLabel: UILabel!
     
     override init(frame: CGRect) {
         self.imageView = UIImageView()
+        self.titleLabel = UILabel()
         super.init(frame: frame)
         configureViews()
         configureConstraints()
@@ -20,19 +22,24 @@ class BranchPromotionSectionHeader: UICollectionReusableView {
     
     func configureViews() {
         addSubview(imageView)
+        addSubview(titleLabel)
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
+        titleLabel.textColor = UIColor.white
     }
     
     func configureConstraints() {
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
         imageView.layer.cornerRadius = 10
         
         NSLayoutConstraint.activate([
             imageView.topAnchor.constraint(equalTo: topAnchor),
             imageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 8),
             imageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -8),
-            imageView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: -18),
+            titleLabel.centerXAnchor.constraint(equalTo: imageView.centerXAnchor)
         ])
     }
     
@@ -40,6 +47,7 @@ class BranchPromotionSectionHeader: UICollectionReusableView {
         guard let x3 = promotion.imageSet?.x3 else { return }
         imageView.backgroundColor = UIColor.hexStringToUIColor(hex: promotion.backgroundColor)
         imageView.downloadImage(from: x3)
+        titleLabel.text = promotion.caption
     }
     
     required init?(coder: NSCoder) {
--- CSClone/Modules/Branches/Cells/PromotionCollectionViewCell.swift
+++ CSClone/Modules/Branches/Cells/PromotionCollectionViewCell.swift
@@ -10,9 +10,11 @@ import UIKit
 class PromotionCollectionViewCell: UICollectionViewCell, SelfConfiguringPromotionCell {
     static let reuseIdentifier: String = "PromotionCollectionViewCell"
     let imageView: UIImageView!
+    let titleLabel: UILabel!
     
     override init(frame: CGRect) {
         self.imageView = UIImageView()
+        self.titleLabel = UILabel()
         super.init(frame: .zero)
         configureViews()
         configureConstraints()
@@ -20,19 +22,24 @@ class PromotionCollectionViewCell: UICollectionViewCell, SelfConfiguringPromotio
     
     func configureViews() {
         contentView.addSubview(imageView)
+        addSubview(titleLabel)
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
+        titleLabel.textColor = UIColor.white
     }
     
     func configureConstraints() {
         imageView.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
         imageView.layer.cornerRadius = 10
         
         NSLayoutConstraint.activate([
             imageView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
             imageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
             imageView.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor),
-            imageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor)
+            imageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: imageView.bottomAnchor, constant: -18),
+            titleLabel.centerXAnchor.constraint(equalTo: imageView.centerXAnchor)
         ])
     }
     
@@ -44,5 +51,6 @@ class PromotionCollectionViewCell: UICollectionViewCell, SelfConfiguringPromotio
         guard let prom = promotion, let x3 = prom.imageSet?.x3 else { return }
         imageView.backgroundColor = UIColor.hexStringToUIColor(hex: prom.backgroundColor)
         imageView.downloadImage(from: x3)
+        titleLabel.text = prom.caption
     }
 }
